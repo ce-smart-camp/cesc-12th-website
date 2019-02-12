4cf563a7ff972dfdbd287760bd4895d6aba2bb8e
Tantatorn Suksangwarn <timefear007@gmail.com>
2019-02-12 21:26:52 +0700

    add countdown timer.

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -2,8 +2,8 @@
   <v-container bg fill-height grid-list-md text-xs-center>
     <v-layout row wrap align-center>
       <v-container>
-        <img src="../assets/logo.png" width="450" height="450">
-        <Countdown date="August 15, 2016"></Countdown>
+        <img src="../assets/logo.svg" width="430" height="430">
+        <Countdown passed_date="December 15, 2019"></Countdown>
       </v-container>
     </v-layout>
   </v-container>
--- src/main.js
+++ src/main.js
@@ -6,3 +6,11 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+Vue.filter('two_digits', function (value) {
+  if(value.toString().length <= 1)
+  {
+      return "0"+value.toString();
+  }
+  return value.toString();
+});
