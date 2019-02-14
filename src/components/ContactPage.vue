b31f9ff1b9367ec7497201ef58ae354cacfa1031
DrZin <61010707@kmitl.ac.th>
2019-02-14 18:34:18 +0700

    [#002] Google Map Layout

--- src/components/ContactPage.vue
+++ src/components/ContactPage.vue
@@ -1,6 +1,10 @@
 <template>
-  <v-container>
-    <google-map/>
+  <v-container fluid fill-height grid-list-md text-xs-center>
+    <v-layout row wrap>
+     <v-flex v-for="i in 2" :key="`6${i}`" xs6>
+        <v-flex v-if="i==2"><google-map/></v-flex>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -1,13 +1,13 @@
 <template>
   <GmapMap
-    :center="{ lat: 10, lng: 10 }"
-    :zoom="7"
+    :center="{ lat: 13.72912977, lng: 100.77560306 }"
+    :zoom="16"
     map-type-id="terrain"
     style="width: 500px; height: 300px"
   >
     <GmapMarker
       :key="index"
-      :position="{ lat: 10, lng: 10 }"
+      :position="{ lat: 13.72912977, lng: 100.77560306 }"
       :clickable="true"
       :draggable="true"
       @click="center = m.position;"
