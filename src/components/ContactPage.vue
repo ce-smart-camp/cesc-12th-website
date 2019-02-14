62f10a5ab068af1d8efe17acf4eed4a126c61b67
DrZin <61010707@kmitl.ac.th>
2019-02-14 18:10:43 +0700

    [#001] google map beta version

---
+++ src/components/ContactPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <google-map/>
+  </v-container>
+</template>
+
+<script>
+import googleMap from "./GoogleMap.vue";
+export default {
+  name: "contactPage",
+  components: {
+    googleMap
+  }
+};
+</script>
+
+<style scoped>
+</style>
+
+
---
+++ src/components/GoogleMap.vue
@@ -0,0 +1,27 @@
+<template>
+  <GmapMap
+    :center="{ lat: 10, lng: 10 }"
+    :zoom="7"
+    map-type-id="terrain"
+    style="width: 500px; height: 300px"
+  >
+    <GmapMarker
+      :key="index"
+      :position="{ lat: 10, lng: 10 }"
+      :clickable="true"
+      :draggable="true"
+      @click="center = m.position;"
+    />
+  </GmapMap>
+</template>
+
+<script>
+export default {
+  name: "googleMap"
+};
+</script>
+
+<style scoped>
+</style>
+
+
