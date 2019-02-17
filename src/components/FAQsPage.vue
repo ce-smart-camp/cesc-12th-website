112c404f6e6af8dee3b00f042cb94bed35e10e70
DrZin <61010707@kmitl.ac.th>
2019-02-17 22:40:15 +0700

    [#004] add vue arrays

--- src/components/FAQsPage.vue
+++ src/components/FAQsPage.vue
@@ -1,16 +1,29 @@
 <template>
   <v-container bg fill-height>
-    <v-layout>
+    <v-layout column>
       <v-flex xs12 sm12 md12>
-        <v-text-field label="FAQs" outline></v-text-field>
+        <v-text-field
+          v-model="search"
+          type="text"
+          label="FAQs"
+          hint="For example, [WIP]"
+          outline
+          clearable
+        ></v-text-field>
       </v-flex>
+      <ul id="example">
+        <li v-for="item in items" :key="item">{{item.message}}</li>
+      </ul>
     </v-layout>
   </v-container>
 </template>
 
 <script>
 export default {
-  name: "FAQsPage"
+  name: "FAQsPage",
+  data() {
+    return { items: [{ message: "Foo" }, { message: "Bar" }] };
+  }
 };
 </script>
 <style scoped>
