2a44892996e743d26679868b0a80332e51efa889
DrZin <61010707@kmitl.ac.th>
2019-02-19 09:45:02 +0700

    templete faqs page

---
+++ src/components/FAQsPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+    <v-layout>
+      <v-flex></v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "FAQsPage",
+  data() {
+    return {
+      blaogs: []
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
+
+
