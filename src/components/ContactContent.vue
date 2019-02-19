d510b13d08735ffdf1d0467dbc3e34a2d2be5823
DrZin <61010707@kmitl.ac.th>
2019-02-19 18:49:33 +0700

    edit link on click icon

--- src/components/ContactContent.vue
+++ src/components/ContactContent.vue
@@ -3,9 +3,15 @@
     <h1>Contact us</h1>
     <p>{{ lorem }}</p>
     <v-layout pt-4 align-center justify-space-around>
-      <v-icon large>fab fa-line</v-icon>
-      <v-icon large>fab fa-facebook</v-icon>
-      <v-icon large>fab fa-instagram</v-icon>
+      <v-btn flat>
+        <v-icon large>fab fa-line</v-icon>
+      </v-btn>
+      <v-btn flat href="https://www.facebook.com/cesckmitl/" target="_blank">
+        <v-icon large>fab fa-facebook</v-icon>
+      </v-btn>
+      <v-btn flat>
+        <v-icon large>fab fa-instagram</v-icon>
+      </v-btn>
     </v-layout>
   </v-container>
 </template>
